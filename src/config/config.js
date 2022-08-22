ae3ef3ab6bd777b7647a879fad73f86cdaf2df41
Vitor Rocha <vitor.rocha@igcp.com.br>
2022-08-22 10:54:32 -0300

    :sparkles: feat: sequelize/postgres config

---
+++ src/config/config.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    host: process.env.DEV_DATABASE_URL,
+    dabase: process.env.DEV_DATABASE_NAME,
+    username: process.env.DEV_DATABASE_USERNAME,
+    password: process.DEV_DATABASE_PASSWORD,
+    dialect: "postgres",
+  },
+  production: {
+    host: process.env.PROD_DATABASE_URL,
+    dabase: process.env.PROD_DATABASE_NAME,
+    username: process.env.PROD_DATABASE_USERNAME,
+    password: process.PROD_DATABASE_PASSWORD,
+    dialect: "postgres",
+  },
+};
