1e9e7248419e545643e449ac4bf48b1b7d911f2b
Vitor Rocha <vitor.rocha@igcp.com.br>
2022-08-25 11:10:50 -0300

    :bug: fix: migration with db

--- src/config/config.js
+++ src/config/config.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    host: process.env.DEV_DATABASE_URL,
-    dabase: process.env.DEV_DATABASE_NAME,
+    host: "127.0.0.1",
+    database: process.env.DEV_DATABASE,
     username: process.env.DEV_DATABASE_USERNAME,
-    password: process.DEV_DATABASE_PASSWORD,
+    password: process.env.DEV_DATABASE_PASSWORD,
     dialect: "mysql",
   },
   production: {
     host: process.env.PROD_DATABASE_URL,
-    dabase: process.env.PROD_DATABASE_NAME,
+    database: process.env.PROD_DATABASE_NAME,
     username: process.env.PROD_DATABASE_USERNAME,
     password: process.PROD_DATABASE_PASSWORD,
-    dialect: "postgres",
+    dialect: "mysql",
   },
 };
