338699c460be581d0535edcff5116ab300bbd1b5
Vitor Rocha <vitor.rocha@igcp.com.br>
2022-08-25 15:47:57 -0300

    :sparkles: feat: config validation

--- src/controllers/AuthControllers.js
+++ src/controllers/AuthControllers.js
@@ -1,10 +1,48 @@
-const db = require("../models");
-const Sequelize = require("sequelize");
+const database = require("../models");
 const errorHandler = require("../helpers/errorHandler");
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const bcrypt = require("bcrypt");
 const logger = require("../config/logger");
 
+// RESOLVEU O VALIDATE E FALTA AS AUTENTICAÇÕES E RBAC
+
+const saltRounds = 10;
+
 const RegisterUser = async (req, res, next) => {
-  res.send({ message: "oi" });
+  database.Registers.removeAttribute("id");
+  try {
+    const user = await database.Registers.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (user) {
+      return errorHandler(StatusCodes.CONFLICT, "Usuário já cadastrado!", res);
+    } else {
+      const encryptedPassword = await bcrypt.hash(
+        req.body.password,
+        saltRounds
+      );
+
+      const Register = await database.Registers.create({
+        email: req.body.email,
+        senha: encryptedPassword,
+      });
+
+      res.send();
+    }
+  } catch (err) {
+    logger.error(err);
+
+    return errorHandler(
+      StatusCodes.NOT_IMPLEMENTED,
+      ReasonPhrases.NOT_IMPLEMENTED,
+      res
+    );
+  }
 };
 
-module.exports = { RegisterUser };
+const LoginUser = async (req, res, next) => {
+  res.send({ message: "ola" });
+};
+
+module.exports = { RegisterUser, LoginUser };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { RegisterUser } = require("../controllers/AuthControllers");
+const { RegisterUser, LoginUser } = require("../controllers/AuthControllers");
+const validate = require("../middlewares/validate");
+const createUser = require("../validations/user.validation");
 
 const router = express.Router();
 
-router.post("/register", RegisterUser);
+router.post("/register", validate(createUser), RegisterUser);
+
+router.post("/login", LoginUser);
 
 module.exports = router;
