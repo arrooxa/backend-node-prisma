06ea3d5b6a5988021d35f97834ac64673e98bc7a
CaioArgIGC <caio.oliveira@igcp.com.br>
2022-08-29 11:05:00 -0300

    :sparkles: feat: logging errors when they are thrown

--- src/middlewares/errorTranslator.js
+++ src/middlewares/errorTranslator.js
@@ -1,6 +1,8 @@
 const errorHandlersRegistry = require('src/error/errorHandlersRegistry');
+const logger = require('../config/logger');
 
 module.exports.errorTranslator = (err, req, res) => {
+    logger.error(err.message);
     const handler = errorHandlersRegistry.get(err.constructor.name) ?? errorHandlersRegistry.get(Error.name);
     const { status, payload } = handler(err);
     res.status(status).send(payload);
