834d2a6baa884e5d198aa8eba9158d41d90b019f
Vitor Rocha <vitor.rocha@igcp.com.br>
2022-08-29 17:19:25 -0300

    :bug: fix: swagger route

--- src/routes/index.js
+++ src/routes/index.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
-  app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   defineLogByEnvironment(app);
 
